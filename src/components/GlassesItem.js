8b95683569811828e836fa60f8f435f9dfe849a3
vuongvgc <vuongvgc@gmail.com>
2020-12-10 20:34:50 +0700

    build function try glass: fix inital

--- src/components/GlassesItem.js
+++ src/components/GlassesItem.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 class GlassesItem extends Component {
   render() {
-    return (
-      <div>
-        <img className="img-fluid" src={this.props.url} alt="glasses" />
-      </div>
-    );
+    if (this.props.url) {
+      return (
+        <div>
+          <img className="img-fluid" src={this.props.url} alt="glasses" />
+        </div>
+      );
+    } else {
+      return "";
+    }
   }
 }
 
